03c3cca633d3d5de94ee08fd47b079469caaff72
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-11-17 18:17:59 +0300

    completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
